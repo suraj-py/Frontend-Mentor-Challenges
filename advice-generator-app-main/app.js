e3a56f0051e6f6ebf52af140543da3473a6b60e9
Suraj Mhatre <surajmcodes@gmail.com>
2023-09-14 00:39:44 +0530

    Project completed

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -1,1 +1,15 @@
-console.log('Its workking')
+const quote = document.querySelector('.advice')
+const refreshButton = document.querySelector('.refresh__button')
+const adviceID = document.querySelector('.id')
+
+function newQuote() {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.slip)
+            adviceID.textContent = data.slip.id
+            quote.textContent = `"${data.slip.advice}"`
+    })
+}
+
+refreshButton.addEventListener('click', newQuote)
