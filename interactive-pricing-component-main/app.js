8174394a1467a1fab7739155598c06b5219bb2c4
Suraj Mhatre <surajmcodes@gmail.com>
2023-09-24 21:41:14 +0530

    Custom range slider

--- interactive-pricing-component-main/app.js
+++ interactive-pricing-component-main/app.js
@@ -1,7 +1,24 @@
 const toggleButton = document.querySelector('.toggle__button')
 const toggleBall = document.querySelector('.ball')
 
+const rangeInputs = document.querySelectorAll('input[type="range"]')
 
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
 
 toggleButton.addEventListener('click', () => {
     toggleBall.classList.toggle('toggle')
