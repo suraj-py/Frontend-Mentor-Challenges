c8438661540d81409c1e7ac18c989b23b05c5208
Suraj Mhatre <surajmcodes@gmail.com>
2023-11-28 01:50:58 +0530

    style: styles added to search bar

--- REST Countries/src/components/FilterRegion.jsx
+++ REST Countries/src/components/FilterRegion.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function FilterRegion() {
   return (
-    <div>FilterRegion</div>
+    <div>
+       <select>
+          <option value="">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+    </div>
   )
 }
 
--- REST Countries/src/components/Searchbar.jsx
+++ REST Countries/src/components/Searchbar.jsx
@@ -1,14 +1,39 @@
+import { Search } from 'lucide-react'
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  padding-left: 10px;
+  background-color: hsl(209, 23%, 22%);
+`;
+
+const SearchInput = styled.input`
+  width: 350px;
+  height: 40px;
+  outline: none;
+  border: none;
+  color: white;
+  padding-left: 10px;
+  font-size: 1rem; 
+  background-color: hsl(209, 23%, 22%);
+
+`;
 
 function Searchbar({name}) {
   return (
-    <form>
-          <input
+    <Form>
+          <Search size={16} color='white' />
+          <SearchInput
               value={name}
               type='text'
               placeholder='Search for a country...'
           />
-    </form>
+    </Form>
   )
 }
 
